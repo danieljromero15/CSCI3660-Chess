23bbd41edd349bd016903430f3306986b7e13f25
danieljromero15 <danieljromero305@gmail.com>
2024-03-09 19:08:27 -0500

    Added pieces and setup

--- app/src/main/java/com/danielromero/chess/MainActivity.java
+++ app/src/main/java/com/danielromero/chess/MainActivity.java
@@ -1,19 +1,15 @@
 package com.danielromero.chess;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.GridLayout;
-import android.widget.GridView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    View[][] chessGrid = new View[8][8];
+    ImageView[][] chessGrid = new ImageView[8][8];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +23,84 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-
+        newGame();
 
     }
 
     private void printDebug(View view) {
-        Log.d("view", String.valueOf(view));
+        String viewString = String.valueOf(view);
+        String square = viewString.substring(viewString.lastIndexOf("app:id/") + 7, viewString.length() - 1);
+        Log.d("square", square);
     }
 
-    private String getIDfromNums(int x, int y){
-        return String.valueOf((char)(x + 97)) + String.valueOf(y + 1);
+    private String getIDfromNums(int x, int y) {
+        return String.valueOf((char) (x + 97)) + String.valueOf(y + 1);
+    }
+
+    public void newGame(){
+        // initial setup
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                String currentSquare = getIDfromNums(i, j);
+                switch(currentSquare){
+                    case "a1":
+                    case "h1":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_rook_white);
+                        break;
+                    case "a8":
+                    case "h8":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_rook_black);
+                        break;
+                    case "b1":
+                    case "g1":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_knight_white);
+                        break;
+                    case "b8":
+                    case "g8":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_knight_black);
+                        break;
+                    case "c1":
+                    case "f1":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_bishop_white);
+                        break;
+                    case "c8":
+                    case "f8":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_bishop_black);
+                        break;
+                    case "d1":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_queen_white);
+                        break;
+                    case "d8":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_queen_black);
+                        break;
+                    case "e1":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_king_white);
+                        break;
+                    case "e8":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_king_black);
+                        break;
+                    case "a2":
+                    case "b2":
+                    case "c2":
+                    case "d2":
+                    case "e2":
+                    case "f2":
+                    case "g2":
+                    case "h2":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_pawn_white);
+                        break;
+                    case "a7":
+                    case "b7":
+                    case "c7":
+                    case "d7":
+                    case "e7":
+                    case "f7":
+                    case "g7":
+                    case "h7":
+                        chessGrid[i][j].setImageResource(R.drawable.chess_piece_pawn_black);
+                        break;
+                }
+            }
+        }
     }
 }
