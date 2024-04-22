5dc77c3c42196b04cbb90bd3d8470abbe6bb79c5
Daniel Romero <danieljromero305@gmail.com>
2024-04-22 05:11:01 -0400

    Updated to 0.1.1 version code 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.danielromero.chess"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1"
+        versionCode = 3
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
