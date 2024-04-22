3dc07f5b75a337268f6ac95f0ad13ac18a8b5de3
Aiden <aiden.p.tobin@gmail.com>
2024-04-21 20:36:18 -0400

    Trying to get a load game function to work

--- app/src/main/java/com/danielromero/chess/Storage.java
+++ app/src/main/java/com/danielromero/chess/Storage.java
@@ -2,27 +2,48 @@ package com.danielromero.chess;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 public class Storage {
     private static SharedPreferences sharedPrefs;
     private static SharedPreferences.Editor edit;
+    private static Chess mChess;
 
     private Storage() {
     }
 
-    public static void make(Context context) { //Use this to initialize the Storage class, don't make an object
+    //Use this to initialize the Storage class, don't make an object
+    //Call Storage.whatever() to implement things
+    public static void make(Context context, Chess bingus) {
         if (sharedPrefs == null) {
             sharedPrefs = context.getSharedPreferences("stats", Context.MODE_PRIVATE);
         }
         edit = sharedPrefs.edit();
+        mChess = bingus;
         vibeCheck();
     }
 
     //Initializing the numbers imma work with, probably boring and long
     //Alternatively call this to reset for testing
+    //No, I will not rename this
     public static void vibeCheck(){
         if(getInt("wPAWN") == -1){
-            edit.putString("Board", "00"); //TODO set initial board state, don't know how im gonna declare peices yet
+            edit.putString("Board", "bROOK,bKNIGHT,bBISHOP,bQUEEN,bKING,bBISHOP,bKNIGHT,pROOK," + //my apolocheese if this is annoying to read
+                            "bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN," +
+                            "00,00,00,00,00,00,00,00," +
+                            "00,00,00,00,00,00,00,00," +
+                            "00,00,00,00,00,00,00,00," +
+                            "00,00,00,00,00,00,00,00," +
+                            "wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN," +
+                            "wROOK,wKNIGHT,wBISHOP,wQUEEN,wKING,wBISHOP,wKNIGHT,wROOK");
+            edit.putString("Board", "bROOK,bKNIGHT,bBISHOP,bQUEEN,bKING,bBISHOP,bKNIGHT,pROOK," + //my apolocheese if this is annoying to read
+                    "bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN," +
+                    "00,wKING,00,00,00,00,00,00," +
+                    "00,00,00,bPAWN,00,00,00,00," +
+                    "00,00,00,00,00,wRook,00,00," +
+                    "00,00,00,00,00,00,00,00," +
+                    "wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN," +
+                    "wROOK,wKNIGHT,wBISHOP,wQUEEN,wKING,wBISHOP,wKNIGHT,wROOK");
             edit.putInt("wPAWN", 0);
             edit.putInt("wROOK", 0);
             edit.putInt("wKNIGHT", 0);
@@ -36,16 +57,82 @@ public class Storage {
         }
     }
 
+    //Use to parse the "Board" sharedpref because I can't store arrays (tragic, I know)
+    public static String[] theParsening(String key){
+        String[] gaming = getString(key).split(",");
+        return gaming;
+    }
+
+    //Heavily modified version of the newGame() from Chess.java to take the string and make pieces
+    public static void setBoard(){
+        String[] board = theParsening(getString("Board"));
+        for(int i = 0; i < board.length; i++){
+            Log.w("test", board[i]);
+        }
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                int pos = i+j;
+                //mChess.setChessPieces(null, i, j);
+                //mChess.getChessSquare(i,j).setImageDrawable(null);
+
+                ChessPiece piece = null;
+                String square = Chess.getIDfromNums(i,j);
+                switch(board[pos]){
+                    case "wPAWN":
+                        piece = new ChessPiece(square, Chess.pieceName.wPAWN);
+                        break;
+                    case "wKNIGHT":
+                        piece = new ChessPiece(square, Chess.pieceName.wKNIGHT);
+                        break;
+                    case "wROOK":
+                        piece = new ChessPiece(square, Chess.pieceName.wROOK);
+                        break;
+                    case "wBISHOP":
+                        piece = new ChessPiece(square, Chess.pieceName.wBISHOP);
+                        break;
+                    case "wKING":
+                        piece = new ChessPiece(square, Chess.pieceName.wKING);
+                        break;
+                    case "wQUEEN":
+                        piece = new ChessPiece(square, Chess.pieceName.wQUEEN);
+                        break;
+                    case "bPAWN":
+                        piece = new ChessPiece(square, Chess.pieceName.bPAWN);
+                        break;
+                    case "bKNIGHT":
+                        piece = new ChessPiece(square, Chess.pieceName.bKNIGHT);
+                        break;
+                    case "bROOK":
+                        piece = new ChessPiece(square, Chess.pieceName.bROOK);
+                        break;
+                    case "bBISHOP":
+                        piece = new ChessPiece(square, Chess.pieceName.bBISHOP);
+                        break;
+                    case "bKING":
+                        piece = new ChessPiece(square, Chess.pieceName.bKING);
+                        break;
+                    case "bQUEEN":
+                        piece = new ChessPiece(square, Chess.pieceName.bQUEEN);
+                        break;
+                    case "00":
+                }
+                mChess.setPiece(piece);
+            }
+
+
+        }
+    }
+
     //getters, If you see the default value something is wrong (panic)
     //Any int returns should never be negative under any circumstance
     public static String getString(String key) {
-        return sharedPrefs.getString(key, "defaultValue");
+        return sharedPrefs.getString(key, "Jumpscare");
     }
     public static int getInt(String key) {
         return sharedPrefs.getInt(key, -1);
     }
 
-    //setters, don't use the int one thx
+    //setters, don't use the int one thxvm
     public static void setString(String key, String string){
         edit.putString(key, string);
         edit.apply();
@@ -55,7 +142,7 @@ public class Storage {
         edit.apply();
     }
 
-    //counters, use these to update integers
+    //counter, use this to bump integers
     public static void upCount(String piece){
         edit.putInt(piece, getInt(piece)+1);
         edit.apply();
